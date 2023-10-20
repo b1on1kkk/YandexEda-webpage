55518616fffe36dcb3747b670dc57f31eb14bfc2
Alexander <114521829+b1on1kkk@users.noreply.github.com>
2023-10-20 21:44:47 +0300

    Delete backend directory

--- backend/global/db_connection.ts
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql");
-
-// Create a MySQL connection
-export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "yandexeda",
-});
